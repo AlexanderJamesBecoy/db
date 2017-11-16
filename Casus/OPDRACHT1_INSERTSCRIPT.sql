45de2d9dad262b25fc6d5ade2399f3ce895d1e2e
Alexander James Becoy <bec_alexanderjames@outlook.com>
2017-11-16 18:25:39 +0100

    v1.6

--- Casus/OPDRACHT1_INSERTSCRIPT.sql
+++ Casus/OPDRACHT1_INSERTSCRIPT.sql
@@ -12,30 +12,66 @@ DELETE FROM Customer
 DELETE FROM Watchhistory
 */
 
-INSERT INTO Person VALUES 
+INSERT INTO Person VALUES
 	(83616, 'Wachowski', 'Andy', 'M'),
 	(83617, 'Wachowski', 'Larry', 'M'),
 	(393411, 'Reeves', 'Keanu', 'M'),
 	(729933, 'Moss', 'Carrie-Anne', 'F'),
-	(151786, 'Fishburne', 'Laurence', 'M')
+	(151786, 'Fishburne', 'Laurence', 'M'),
+	(121111, 'Anthony', 'Ray', 'M'),
+	(121112, 'Anu', 'Christine', 'F'),
+	(121113, 'Alice', 'Mary', 'F'),
+	(121114, 'Atwal', 'Tanveer K.', 'F')
+	(123143, 'Darabont', 'Frank', 'M'),
+	(123145, 'Robbens', 'Tim', 'M'),
+	(123144, 'Freeman', 'Morgan', 'M'),
+	(123146, 'Gunton', 'Bob', 'M'),
+	(123147, 'Nolan', 'Christopher', 'M'),
+	(123148, 'Bale', 'Christian', 'M'),
+	(123149, 'Ledger', 'Heath', 'M'),
+	(123150, 'Eckhart', 'Aaron', 'M')
 
-INSERT INTO Movie VALUES 
-	(11, 'Matrix, The', 122, 'Description of Matrix, The', 1999, NULL, NULL, 2.50, 'http...'),
-	(12, 'Matrix Reloaded, The', 106, 'Description of Matrix Reloaded, The', 2003, NULL, 11, 2.50, 'http...'),
-	(13, 'Matrix Revolutions, The', 103, 'Description of Matrix Revolutions, The', 2003, NULL, 12, 2.50, 'http...')
 
-INSERT INTO Movie_Directors VALUES 
+INSERT INTO Movie VALUES
+	(11, 'Matrix, The', 122,
+		'Description of Matrix, The', 1999, NULL, NULL, 2.50, 'https://www.youtube.com/watch?v=m8e-FF8MsqU'),
+	(12, 'Matrix Reloaded, The', 106,
+		'Description of Matrix Reloaded, The', 2003, NULL, 11, 2.50, 'https://www.youtube.com/watch?v=kYzz0FSgpSU'),
+	(13, 'Matrix Revolutions, The', 103,
+		'Description of Matrix Revolutions, The', 2003, NULL, 12, 2.50, 'https://www.youtube.com/watch?v=hMbexEPAOQI')
+	(14, 'Shawshank Redemption, The', 144,
+		'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+		1995, 2.50, 'https://www.youtube.com/watch?v=6hB3S9bIaco')
+	(15, 'Dark Knight, The', 152,
+		'When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham, the Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+		2008, 2.50, 'https://www.youtube.com/watch?v=EXeTwQWrcwY')
+
+INSERT INTO Movie_Directors VALUES
 	(11, 83616),
 	(11, 83617),
 	(12, 83616),
-	(12, 83617)
+	(12, 83617),
+	(13, 83616),
+	(13, 83617),
+	(14, 123143),
+	(15, 123147)
 
-INSERT INTO Movie_Cast VALUES 
+INSERT INTO Movie_Cast VALUES
 	(11, 393411, 'Neo'),
 	(11, 729933, 'Trinity'),
-	(11, 151786, 'Morpheus')
+	(11, 151786, 'Morpheus'),
+	(12, 121111, 'Power Station Guard'),
+	(12, 121112, 'Kali'),
+	(13, 121113, 'The Oracle'),
+	(13, 121114, 'Sati'),
+	(14, 123145, 'Andy Dufresne'),
+	(14, 123144, 'Red'),
+	(14, 123146, 'Warden Norton'),
+	(15, 123148, 'Bruce Wayne'),
+	(15, 123149, 'Joker'),
+	(15, 123150, 'Harvey Dent')
 
-INSERT INTO Genre VALUES 
+INSERT INTO Genre VALUES
 	('Action', 'Creative works characterized by emphasis on exciting action sequences'),
 	('Drama', 'Fictional division between comedy and tragedy'),
 	('Family', 'Movies suitable for a wide range of age groups'),
@@ -43,13 +79,20 @@ INSERT INTO Genre VALUES
 
 INSERT INTO Movie_Genre VALUES
 	(11, 'Action'),
-	(11, 'Sci-Fi')
+	(11, 'Sci-Fi'),
+	(12, 'Action'),
+	(12, 'Sci-Fi'),
+	(13, 'Action'),
+	(13, 'Sci-Fi'),
+	(14, 'Drama'),
+	(15, 'Action'),
+	(15, 'Drama')
 
-INSERT INTO Country VALUES 
+INSERT INTO Country VALUES
 	('The Netherlands'),
 	('Belgium')
 
-INSERT INTO Payment VALUES 
+INSERT INTO Payment VALUES
 	('Mastercard'),
 	('Visa'),
 	('Amex')
